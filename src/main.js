7b3e2160d0a29fe69fe4bee3ee173cd044e7e816
Shaaz Umer <shaazumer066@gmail.com>
2026-09-07 06:52:57 +0530

    Add dynamic time-based greeting

--- src/main.js
+++ src/main.js
@@ -122,6 +122,26 @@ function updateClock() {
 updateClock();
 setInterval(updateClock, 1000);
 
+function updateGreeting() {
+  const hour = new Date().getHours();
+  let greeting;
+
+  if (hour < 12) {
+    greeting = 'Good morning';
+  } else if (hour < 18) {
+    greeting = 'Good afternoon';
+  } else if (hour < 22) {
+    greeting = 'Good evening';
+  } else {
+    greeting = 'Good night';
+  }
+
+  document.querySelector('.greeting-text').textContent = greeting;
+}
+
+updateGreeting();
+setInterval(updateGreeting, 60000);
+
 const searchForm = document.querySelector('#search-form');
 const searchInput = document.querySelector('#search-input');
 
